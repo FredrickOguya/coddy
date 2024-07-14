c4c81ac10455405f787fdab7080c0dac16902726
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-14 14:58:57 +0300

    Hello world

---
+++ Introduction To Javascript/1 Introduction/1.2 Hello world.js
@@ -0,0 +1,43 @@
+/*
+Hello World!
+
+The "Hello World!" is a simple program that outputs Hello World! to the screen.
+
+In JavaScript, we use console.log() to show words on the screen. The words go inside quotation marks.
+
+Let's take a look at the "Hello World!" program in JavaScript:
+
+console.log("Hello World!")
+
+Quiz
+
+2 Questions
+Start
+
+
+Challenge
+
+Beginner
+Use the code view to write a program that outputs Hello World!
+
+Note that anything inside quotation marks is case sensitive. For example:
+
+console.log("Hello World!")
+console.log("hello world!")
+are different things (notice the capital letters in the first line).
+
+
+Hints
+
+Hint 1
+
+
+Copy the example code!
+*/
+
+Solution
+
+
+console.log("Hello World!")
+
+
