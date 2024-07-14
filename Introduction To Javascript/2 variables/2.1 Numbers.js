6154f23d5b7796cc1824c932eff387105d4c2590
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-14 15:05:07 +0300

    numbers

---
+++ Introduction To Javascript/2 variables/2.1 Numbers.js
@@ -0,0 +1,42 @@
+/*
+Numbers
+
+Variables are containers that hold data values. They are used to store, manipulate, and display information within a program.
+
+In short, a variable is like a memory unit that we can access by typing the name of the variable.
+
+Each variable has a unique name and a value that can be of different types. In JavaScript, we use the let keyword to declare variables.
+
+To initialize a variable, we use the following format:
+
+let variableName = value;
+In JavaScript, there's only one numeric data type: Number. It can represent both integers and floating-point numbers.
+
+For example:
+
+To initialize a variable with an integer value:
+
+let a = 3;
+To initialize a variable with a floating-point value:
+
+let b = 13.2;
+JavaScript automatically handles the number type, whether it's an integer or a floating-point number.
+
+
+Quiz
+
+2 Questions
+
+Done
+
+Challenge
+
+Beginner
+Write code that initialize a variable named coddyVar with the value 5.
+*/
+
+Solution
+
+
+
+let coddyVar = 5
