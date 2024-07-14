31a1dd2e66a94b56709d1c6bf00d51eeaa693eee
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-07-14 14:40:42 +0300

    introduction.

---
+++ Introduction To Javascript/1 Introduction/1.1 introduction.js
@@ -0,0 +1,15 @@
+/*
+JavaScript is a versatile and widely-used programming language that powers the interactive elements of websites. This course will teach you the basics of JavaScript, without requiring any prior knowledge.
+
+
+Challenge
+
+Beginner
+Press the run code button to run your first code in JavaScript
+
+*/
+Solution
+
+console.log("Hello JavaScript!")
+
+
