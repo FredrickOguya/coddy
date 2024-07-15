e97fe2d0d72f81e337015b10fb5223c1614b2710
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-15 13:21:15 +0300

    push method test

---
+++ Array Methods in Javascript/working.js
@@ -0,0 +1,15 @@
+/*
+let candyBasket = ['red','orange','yellow','green','blue']
+
+
+console.log(candyBasket[(candyBasket.length-1)/2])
+
+const cupcakes = ["Vanilla", "Chocolate", "Red Velvet", "Lemon"];
+
+// Write code here
+console.log(cupcakes.length)
+*/
+let playlist = ['Uptown Funk', 'Happy', 'Downtown'];
+let removed= playlist.pop()
+playlist.push('Dynamite')
+console.log(playlist)
