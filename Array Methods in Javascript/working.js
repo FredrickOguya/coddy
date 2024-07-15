757f5415b75c13e4a2f9c21213ca076721b607ae
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-15 13:27:32 +0300

    shift and unshift test

--- Array Methods in Javascript/working.js
+++ Array Methods in Javascript/working.js
@@ -8,8 +8,16 @@ const cupcakes = ["Vanilla", "Chocolate", "Red Velvet", "Lemon"];
 
 // Write code here
 console.log(cupcakes.length)
-*/
+
 let playlist = ['Uptown Funk', 'Happy', 'Downtown'];
 let removed= playlist.pop()
 playlist.push('Dynamite')
 console.log(playlist)
+*/
+
+let tasks = ['Read a book', 'Play sports', 'Wash dishes'];
+
+// Write code here
+let removed = tasks.shift();
+ tasks.unshift('Feed the cat')
+console.log(tasks)
